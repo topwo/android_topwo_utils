e4b95fcb6610fcc5500fabad8ed3d4e17fb3bef8
lx <634429286@qq.com>
2022-01-18 15:10:04 +0800

    +分辨率转换

--- topwoutils/app/src/main/java/com/topwo/utils/TopwoSystem.java
+++ topwoutils/app/src/main/java/com/topwo/utils/TopwoSystem.java
@@ -228,4 +228,21 @@ public class TopwoSystem {
         }
         return 0;
     }
+
+
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dpToPx(Context context, final float dp) {
+        return (int) (dp * context.getResources().getDisplayMetrics().density);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int pxToDp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 }
