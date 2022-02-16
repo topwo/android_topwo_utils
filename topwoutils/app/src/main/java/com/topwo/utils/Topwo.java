5f92f540c5e0268a9182a8e26f066b33382fc1dc
lx <634429286@qq.com>
2022-02-16 20:42:05 +0800

    +所有的初始化集中到Topwo里

--- topwoutils/app/src/main/java/com/topwo/utils/Topwo.java
+++ topwoutils/app/src/main/java/com/topwo/utils/Topwo.java
@@ -1,5 +1,22 @@
 package com.topwo.utils;
 
+import android.app.Application;
+import android.content.Context;
+
 public class Topwo {
     private static final String TAG = Topwo.class.getSimpleName();
+
+    private static Application sApplication = null;
+    private static Context sContext = null;
+
+    public static void init(Application application) {
+        sApplication = application;
+        sContext = sApplication.getApplicationContext();
+
+        TopwoFile.init(sContext);
+        TopwoLog.init(sContext);
+        TopwoSharedPreferences.init(sContext);
+        TopwoSystem.init(sContext);
+        TopwoUri.init(sContext);
+    }
 }
